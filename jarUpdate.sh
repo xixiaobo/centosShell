3b1ed1aae0f5a1b64d195be0e5f43c83fa572ce5
我家猫每天都在飞 <34016667+xixiaobo@users.noreply.github.com>
2021-12-08 12:10:36 +0800

    Update jarUpdate.sh

--- jarUpdate.sh
+++ jarUpdate.sh
@@ -9,84 +9,45 @@ shellName=$(basename $BASH_SOURCE)
 dname=`dirname "$shellPath"`
 
 ## 项目名称 ##
-projectName='cdn_gateway'
+projectName='test'
 ## 项目版本 ##
-projectVersion='1.0-SNAPSHOT'
+projectVersion='1.0.0-SNAPSHOT'
 
 ## 项目git地址 ##
-gitPath='git@118.190.65.73:cdn_cds/cdn_gateway.git'
+gitPath='https://username:password@git@ip:project.git'
+#gitPath='https://username:password@ip:project'
 ## 项目git分支 默认 master##
-gitBranch=
+gitBranch='1.0.0-SNAPSHOT'
 
 ## 项目运行地址，默认脚本所在地址 ##
-projectPath=
+projectPath=$shellPath
 ## 项目源代码路径， 默认将在项目运行目录下创建 source 存储 ##
-javaSourceCodePath=
+javaSourceCodePath=$projectPath/source
 ## 项目jar包，默认项目名称加版本号 ##
-projectJar=
-## 项目进程查询内容，默认项目地址加项目jar包 ##
-processMonitoring=
+projectJar=$projectName-$projectVersion'.jar'
 ## 项目运行日志，默认项目运行地址下+zxfx ##
-runLogPath=
-## 项目运行java分配最小内存，默认 10m ##
-javaXms=
-## 项目运行java分配最da内存，默认 200m ##
-javaXmx=
+runLogPath=${projectPath}/zxfx
+
+## 项目进程查询内容，默认项目地址加项目jar包 ##
+processMonitoring=$projectPath/$projectJar
 
 ## 项目运行命令，默认 java -jar 加 分配内存 加 项目运行地址 加 项目jar包 ##
-startCmd=
+startCmd="java -Xms10m -Xmx200m -jar $projectPath/$projectJar"
+
+## 项目运行类型 nohup（后台运行并写入日志）， daemon（前台运行并写入日志），no（直接运行也不写入日志）##
+startType="nohup"
+
+## 项目停止命令，根据进程id kill ##
+stopCmd="ps -ef|grep '${processMonitoring}' |grep -v grep | awk '{print \$2}' |xargs kill -kill"
+
+## 项目重启命令，默认先停止运行在启动，默认值 no ##
+restartCmd='no'
 
 
-if test -z "$projectPath"
-then
- projectPath=$shellPath
-fi 
-
-if test -z "$javaSourceCodePath"
-then
- javaSourceCodePath=$projectPath/source
-fi 
-
-if test -z "$projectJar"
-then
- projectJar=$projectName-$projectVersion'.jar'
-fi 
-
-if test -z "$runLogPath"
-then
- runLogPath=${projectPath}/zxfx
-fi  
-
-if test -z "$javaXms"
-then
- javaXms=10m
-fi  
-
-if test -z "$javaXmx"
-then
- javaXmx=200m
-fi  
-
-if test -z "$startCmd"
-then
- startCmd="java -Xms$javaXms -Xmx$javaXmx -jar $projectPath/$projectJar"
-fi  
-
-if test -z "$processMonitoring"
-then
- processMonitoring=$startCmd
-fi  
-
-if test -z "$gitBranch"
-then
- gitBranch='master'
-fi  
 
 echo 
-echo -e "\033[34m ### 欢迎使用${shellName}脚本,本次运行的相关项目为: ${projectName} ### \033[0m"
+echo -e "\033[34m ### 欢迎使用 ${shellName} 脚本,本次运行的相关项目为: ${projectName} ### \033[0m"
 echo 
-
-
 
 
 time=$(date "+%Y%m%d%H%M%S")
@@ -98,25 +59,39 @@ check_pid(){
 
 start_project(){
 	check_pid
-	[[ ! -z ${PID} ]] && echo -e "\033[33m 项目 正在运行 !进程PID为 ${PID} \033[0m" && exit 1
+	[[ ! -z ${PID} ]] && echo -e "启动项目：\033[33m 项目 正在运行 !进程PID为 ${PID} \033[0m" && exit 1
 	aw="${*:2}"
 	echo
 	echo -e "\033[44;37m ======================================== \n || startCmd: ${startCmd} ${aw[*]} \n ======================================== \033[0m"
 	echo
 	if test -r "${runLogPath}.log" 
 	then
-		mv "${runLogPath}.log" "${runLogPath}-${time}.log"
+		if [ ! -d ${projectPath}/old-run-log ]; then
+			mkdir -p ${projectPath}/old-run-log
+		fi
+		mv "${runLogPath}.log" "${projectPath}/old-run-log/${time}.log"
 	fi
-	eval "nohup  $startCmd ${aw[*]} >> ${runLogPath}.log  2>&1 &"
-
+	if [[ "$startType" == "nohup" ]];then
+		echo -e "\033[32m 运行模式：后台运行 \033[0m"
+		eval "nohup  $startCmd ${aw[*]} >> ${runLogPath}.log  2>&1 &"
+	elif [[ "$startType" == "daemon" ]];then
+		echo -e "\033[32m 运行模式：前台运行 \033[0m"
+		eval "$startCmd ${aw[*]} >> ${runLogPath}.log  2>&1"
+	elif [[ "$startType" == "no" ]];then
+		echo -e "\033[32m 运行模式：直接运行 \033[0m"
+		eval "$startCmd ${aw[*]}"
+	else
+		echo -e "\033[31m 未知运行类型 \033[0m"
+	fi
+	
 	echo -e "\033[44;37m ======================================== \n || log flie: ${runLogPath}.log \n ======================================== \033[0m"
 	echo
 	check_pid
 	if [ -z ${PID} ]
 	then
-		echo -e "\033[32m 项目运行失败，没有找到进程 \033[0m"
+		echo -e "启动项目：\033[32m 项目运行失败，没有找到进程 \033[0m"
 	else
-		echo -e "\033[32m 项目运行成功 PID：${PID} \033[0m" 
+		echo -e "启动项目：\033[32m 项目运行成功 PID：${PID} \033[0m" 
 	fi  
 }
 
@@ -124,25 +99,50 @@ stop_porject(){
 	check_pid
 	if [ -z ${PID} ]
 	then
-		echo -e "\033[33m \n项目 未运行 !\n \033[0m" 
+		echo -e "\n停止运行： \033[33m 项目 未运行 !\n \033[0m" 
 	else
-		ps -ef|grep "$processMonitoring" |grep -v grep | awk '{print $2}' |xargs kill -kill
+		eval "$stopCmd ${aw[*]}"
 		check_pid
 		if [ -z ${PID} ]
 		then
-			echo -e "\033[32m \n项目 退出成功 !\n \033[0m"
+			echo -e "\n停止运行：\033[32m 项目 退出成功 !\n \033[0m"
 		else
-			echo -e "\033[31m \n项目 退出失败 ! 项目运行PID：${PID}\n \033[0m"  && exit 1
+			echo -e "\n停止运行：\033[31m 项目 退出失败 ! 项目运行PID：${PID}\n \033[0m"  && exit 1
 		fi  
 	fi  
 }
 
 restart_project(){
-	stop_porject
-	start_project $*
+	if [[ "$restartCmd" == "no" ]];then
+		stop_porject
+		start_project $*
+	else
+		eval "$restartCmd ${aw[*]}"
+	fi
 }
 
 update_project()
+{
+	init_project
+	git pull
+	mvn clean
+	mvn package
+	if [ $? -eq 0 ]; then
+		if test -r "${projectPath}/${projectJar}" 
+		then
+			if [ ! -d $projectPath/old-jar ]; then
+				mkdir -p $projectPath/old-jar
+			fi
+			mv $projectPath/$projectJar $projectPath/old-jar/${projectJar%.*}-$time.jar
+		fi
+		mv $javaSourceCodePath/$projectName/target/$projectJar $projectPath
+		echo '项目更新成功！'
+	else
+		echo '项目打包失败！'
+	fi
+}
+
+init_project()
 {
 	if [ ! -d $javaSourceCodePath ]; then
 		mkdir -p $javaSourceCodePath
@@ -154,10 +154,11 @@ update_project()
 		echo '项目源代码文件夹不存在'
 		cd $javaSourceCodePath
 		echo '正在下载项目源代码'
-		git clone $gitPath
+		git clone $gitPath	
 	fi
 	if [ ! -d $javaSourceCodePath/$projectName ]; then
 		echo '项目代码文件夹未下载成功，请检查原因'
+		exit 1
 	else
 		cd $javaSourceCodePath/$projectName
 		### 检查切换分支 start ####
@@ -180,19 +181,6 @@ update_project()
 			fi 
 		fi 
 		### 检查切换分支 end ####
-		git pull
-		mvn clean
-		mvn package
-		if [ $? -eq 0 ]; then
-			if [ ! -d $projectPath/old-jar ]; then
-				mkdir -p $projectPath/old-jar
-			fi
-			mv $projectPath/$projectJar $projectPath/old-jar/${projectJar%.*}-$time.jar
-			mv $javaSourceCodePath/$projectName/target/$projectJar $projectPath
-			restart_project $*
-		else
-			echo '项目打包失败！'
-		fi
 	fi
 }
 
@@ -227,8 +215,12 @@ case "$1" in
         update_project $*
  ;;
 
+ init)
+        init_project
+ ;;
+
  *)
-   echo "\033[33m Usage: $shellName  {start|stop|restart|status|update} \033[0m"
+   echo -e "\033[33m Usage: $shellName  {start|stop|restart|status|update|init} \033[0m"
  ;;
 
 esac
